0346ff296345b420b8e40aa79fb0f9fa0912bad5
JohnHCarlson <jcjohncarlson@gmail.com>
2022-05-06 11:44:44 -0400

    Added basic routing for main pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import TopNav from "./components/topnav";
+import {Route, Routes} from 'react-router-dom';
+
+import Home from './routes/home';
+import Resume from './routes/resume';
+import Projects from './routes/projects';
+import Pindex from './routes/pindex';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <TopNav/>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/resume" element={<Resume/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/pindex" element={<Pindex/>}/>
+      </Routes>
+    </div>
   );
 }
 
