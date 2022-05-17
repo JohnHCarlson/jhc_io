f87eb518d678ec72738b7a27837966060d42cb31
JohnHCarlson <jcjohncarlson@gmail.com>
2022-05-17 14:14:30 -0400

    Created basic pindex pin card component

---
+++ src/components/pincard.jsx
@@ -0,0 +1,30 @@
+import example_img from '../../public/286x180.png'
+import { Card, Button, Collapse, ListGroup, ListGroupItem } from "react-bootstrap";
+import { useState } from 'react';
+
+function PinCard(props) {
+
+    const [open, setOpen] = useState(false);
+
+    return (
+        <Card style={{width: '18rem'}}>
+            <Card.Img variant="top" src={example_img}/>
+            <Card.Body>
+                <Card.Title>{props.name}</Card.Title>
+                <Card.Subtitle>{props.office} - {props.year}</Card.Subtitle>
+            </Card.Body>
+            {/* Currently only displaying high-level meta data
+            <Button onClick={() => setOpen(!open)}>More Info</Button>
+            <Collapse in={open}>
+                <ListGroup className="list-group-flush">
+                    <ListGroupItem>Cras justo odio</ListGroupItem>
+                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                </ListGroup>
+            </Collapse>
+            */}
+        </Card>
+    );
+  }
+  
+  export default PinCard;
