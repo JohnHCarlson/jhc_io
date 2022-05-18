58b8460713aa3f4f6ce1ad298424697bfe07363f
JohnHCarlson <jcjohncarlson@gmail.com>
2022-05-17 22:21:03 -0400

    Added ability to populate pins from API

--- src/components/pindexAlbum.jsx
+++ src/components/pindexAlbum.jsx
@@ -1,25 +1,32 @@
+import { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
+import { getAllPins } from "../services/pindexService";
+import PinCard from "./pincard";
 
-function PindexAlbum(pins) {
-
-    const PinCard = (pin, index) => {
-        return(
-            <PinCard name={pin.name} office={pin.office} year={pin.year}/>
-        )
-    }
-
-    const pinTable = pins.map((pin, index) => PinCard(pin, index))
-
-
-    return (
-      <div className="py-5 bg-light">
-          <Container>
-              <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                {pinTable}
-              </Row>
-          </Container>
-      </div>
-    );
-  }
+function PindexAlbum(){
   
-  export default PindexAlbum;
+  const [pins, setPins] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getAllPins()
+    .then(items => {
+      if(mounted){
+        setPins(items)
+      }
+    })
+    return () => mounted = false;
+  })
+
+  return (
+    <div className="py-5 bg-light">
+        <Container>
+            <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              {pins.map(item => <PinCard name={item.candidate_name} office={item.office} year={2022}/>)}
+            </Row>
+        </Container>
+    </div>
+  );
+}
+
+export default PindexAlbum;
