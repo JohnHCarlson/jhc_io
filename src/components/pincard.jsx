7c9fcabf873e94c50d615b67aad90bc19130942a
JohnHCarlson <jcjohncarlson@gmail.com>
2022-05-19 13:23:42 -0400

    Updated pincard props handling

--- src/components/pincard.jsx
+++ src/components/pincard.jsx
@@ -1,26 +1,45 @@
 import example_img from '../test/286x180.png'
 import { Card, Button, Collapse, ListGroup, ListGroupItem } from "react-bootstrap";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function PinCard(props) {
 
+    function prepCanorgNames(canorgs){
+
+        let canorgNameList = [];
+
+        canorgs.map(canorg =>{
+            canorgNameList.push(canorg.canorg_name);
+        })
+        return canorgNameList;
+    }
+
+    function prepOffices(offices){
+
+        let officeList = [];
+        offices.map(office =>{
+            officeList.push(office.office_name);
+        })
+        return officeList;
+    }
+
+    function prepYear(date){
+        if(date){
+            const year = date.substr(0,4);
+            return year;
+        }
+        return "";
+    }
+
     return (
+        
         <Card style={{width: '18rem'}}>
             <Card.Img variant="top" src={example_img}/>
             <Card.Body>
-                <Card.Title>{props.name}</Card.Title>
-                <Card.Subtitle>{props.office} - {props.year}</Card.Subtitle>
+                <Card.Title>{}</Card.Title>
+                    {prepCanorgNames(props.name).map(name => <Card.Title>{name}</Card.Title> )}
+                    {prepOffices(props.office).map(office => <Card.Subtitle>{office} - {prepYear(props.year)}</Card.Subtitle> )}
             </Card.Body>
-            {/* Currently only displaying high-level meta data
-            <Button onClick={() => setOpen(!open)}>More Info</Button>
-            <Collapse in={open}>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem>Cras justo odio</ListGroupItem>
-                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                </ListGroup>
-            </Collapse>
-            */}
         </Card>
     );
   }
