9cd6b4af7400582c83cb52e69ba38b59b50e31ed
JohnHCarlson <jcjohncarlson@gmail.com>
2022-05-19 13:24:20 -0400

    Added API query to file, updated pin card population ability

--- src/components/pindexAlbum.jsx
+++ src/components/pindexAlbum.jsx
@@ -1,28 +1,33 @@
 import { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
-import { getAllPins } from "../services/pindexService";
 import PinCard from "./pincard";
 
 function PindexAlbum(){
-  
+
   const [pins, setPins] = useState([]);
+  const [queryParams, setQueryParams] = useState({});
+
+  const getPins = async(queryParams) => {
+
+    const response = await fetch('http://127.0.0.1:8000/pindex/pins/' + new URLSearchParams(queryParams));    
+    const data = await response.json();
+
+    setPins(data)
+  }
 
   useEffect(() => {
-    let mounted = true;
-    getAllPins()
-    .then(items => {
-      if(mounted){
-        setPins(items)
-      }
-    })
-    return () => mounted = false;
-  })
+    getPins(queryParams)
+  }, [])
+
+
 
   return (
+    
     <div className="py-5 bg-light">
         <Container>
             <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-              {pins.map(item => <PinCard name={item.candidate_name} office={item.office} year={2022}/>)}
+              {console.log(pins)}
+              {pins.map(item => <PinCard name={item.canorgs} office={item.offices} year={item.election_date}/>)}
             </Row>
         </Container>
     </div>
