195fe50bc77eb7502bc6d5577dccd52c4a464c91
JohnHCarlson <jcjohncarlson@gmail.com>
2022-05-21 12:15:09 -0400

    Updated functionality to add submission modal

--- src/components/pindexCenterContainer.jsx
+++ src/components/pindexCenterContainer.jsx
@@ -1,19 +1,29 @@
-import { Row, Col, Button } from "react-bootstrap";
+import { useState } from "react";
+import { Row, Col, Button, Modal } from "react-bootstrap";
+import PindexModalSubmit from "./pindexModalSubmit";
 
 function PindexCenterContainer() {
-    return (
-      <section className="py-5 text-center">
-          <Row className="py-lg-5">
-            <Col className="lg-6 md-8 mx-auto">
-                <h1 className="fw-light">Album Example</h1>
-                <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-                <div>
-                    <Button className="btn-primary my-2">Submit Pins</Button>
-                </div>
-            </Col>
-          </Row>
-      </section>
-    );
-  }
+
+  const [showSubmit, setShowSubmit] = useState(false);
+
+  const handleClose = () => setShowSubmit(false);
+  const handleShow = () => setShowSubmit(true);
+
+  return (
+    <section className="py-5 text-center">
+        <Row className="py-lg-5">
+          <Col className="lg-6 md-8 mx-auto">
+              <h1 className="fw-light">Album Example</h1>
+              <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+              <div className="">
+                  <Button className="btn-primary my-2" onClick={handleShow}>Submit Pins</Button>
+                  <Button className="btn-primary my-2">Search</Button>
+              </div>
+          </Col>
+        </Row>
+        <PindexModalSubmit show={showSubmit} onHide={handleClose}/>
+    </section>
+  );
+}
   
-  export default PindexCenterContainer;
+export default PindexCenterContainer;
